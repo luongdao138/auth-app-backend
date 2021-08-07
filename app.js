d52c81306b265648f058ae4a63e16bad15c5f3f9
Dao Luong <daovanluongpdl@gmail.com>
2021-08-07 14:43:20 +0700

    complete

--- app.js
+++ app.js
@@ -30,7 +30,8 @@ app.use(
     secret: 'session_secret',
     cookie: {
       maxAge: 1000 * 60 * 60 * 24,
-      sameSite: 'none', // 1 day
+      sameSite: 'none',
+      httpOnly: true, // 1 day
     },
     store: sessionStore,
   })
