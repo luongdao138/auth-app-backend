18cca75516b2faa53670bc2b857c92d44b22ddfe
Dao Luong <daovanluongpdl@gmail.com>
2021-08-08 10:05:46 +0700

    complete

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const cors = require('cors');
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+// const MongoStore = require('connect-mongo');
+const mongodbStore = require('connect-mongo')(session);
 const passport = require('./passport');
 
 app.use(
@@ -13,31 +14,6 @@ app.use(
     credentials: true,
   })
 );
-
-// middlewares
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// session store
-const sessionStore = MongoStore.create({
-  mongoUrl: process.env.MONGODB_URI,
-  collectionName: 'sessions',
-});
-app.use(
-  session({
-    resave: false,
-    saveUninitialized: false,
-    secret: 'session_secret',
-    cookie: {
-      maxAge: 1000 * 60 * 60 * 24,
-    },
-    store: sessionStore,
-  })
-);
-
-// passport
-app.use(passport.initialize());
-app.use(passport.session());
 
 // connect mongodb database
 mongoose.connect(process.env.MONGODB_URI, {
@@ -54,6 +30,44 @@ db.on('error', (error) => {
 db.on('open', () => {
   console.log('connected db successfully!');
 });
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// session store
+// const sessionStore = MongoStore.create({
+//   mongoUrl: process.env.MONGODB_URI,
+//   collectionName: 'sessions',
+// });
+// app.use(
+//   session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: 'session_secret',
+//     cookie: {
+//       maxAge: 1000 * 60 * 60 * 24,
+//     },
+//     store: sessionStore,
+//   })
+// );
+app.use(
+  session({
+    name: 'quicklinks.sess',
+    store: new mongodbStore({
+      mongooseConnection: mongoose.connection,
+      touchAfter: 24 * 3600,
+    }),
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 1000 * 60 * 15 },
+  })
+);
+
+// passport
+app.use(passport.initialize());
+app.use(passport.session());
 
 // routers
 const authRouter = require('./routes/auth');
